c1c5df62e1e2351e938f7473b0b40b10e1e3a3fb
Eira Melé <eira.mele@gmail.com>
2024-04-07 17:57:26 +0200

    Add genre controllers

--- src/controllers/genre.controllers.ts
+++ src/controllers/genre.controllers.ts
@@ -26,6 +26,35 @@ export const getAllGenres = async (req: Request, res: Response) => {
   }
 };
 
-export const updateGenre = async (req: Request, res: Response) => {};
+export const updateGenre = async (req: Request, res: Response) => {
+  const { name } = req.body;
+  const { genreId } = req.params;
+  const updateData: { name?: string } = {};
 
-export const deleteGenre = async (req: Request, res: Response) => {};
+  if (name) updateData.name = name;
+
+  if (Object.keys(updateData).length === 0) {
+    return res.status(400).send({ message: "No update fields provided." });
+  }
+
+  try {
+    const genreUpdated = await GenreModel.findByIdAndUpdate(
+      { _id: genreId },
+      { name },
+      { new: true }
+    ).populate("genre");
+    res.status(201).send(genreUpdated);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
+
+export const deleteGenre = async (req: Request, res: Response) => {
+  const { genreId } = req.params;
+  try {
+    const genreDeleted = await GenreModel.findOneAndDelete({ _id: genreId });
+    res.status(201).send(genreDeleted);
+  } catch (error) {
+    res.status(400).send(error);
+  }
+};
